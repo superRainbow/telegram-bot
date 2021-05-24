4e9e95eb8bc195733aa0281b5a194e5716028714
rainbow <g901612002@hotmail.com>
2021-05-24 10:35:09 +0800

    調整布版設定

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const { Telegraf } = require('telegraf');
-const moment = require('moment');
+const moment = require('moment-timezone');
 const { TIME } = require('./const');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 const init = () => {
-    const isWorkingDay = true;
     const isWorkingDay = moment().isoWeekday() < 6;
     if (isWorkingDay) {
         checkTime();
@@ -13,14 +12,13 @@ const init = () => {
 }
 
 const checkTime = () => {
-    const now = moment(); 
+    const now = moment().tz('Asia/Taipei');
     Object.keys(TIME).map(time => {
-        const booking = time.split(':')
-        if (parseInt(booking[0], 10) === now.hour() && (parseInt(booking[1], 10) - now.minute() < 5 && parseInt(booking[1], 10) - now.minute() > 0)) {
-            bot.telegram.sendMessage( process.env.TELEGRAM_MY_ID, TIME[time]);
+        const booking = time.split(':');
+        if (parseInt(booking[0], 10) === now.hour() && (parseInt(booking[1], 10) - now.minute() < 10 && parseInt(booking[1], 10) - now.minute() > 0)) {
+            bot.telegram.sendMessage( process.env.TELEGRAM_TEAM_ID, TIME[time]);
         }
     });
-
 }
 
 init();
